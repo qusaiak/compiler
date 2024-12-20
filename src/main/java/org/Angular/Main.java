45d2b0203febf2295fad7230b279ce3181d0adda
qusai <qusai2000abokhier@gmail.com>
2024-12-20 21:38:41 +0300

    update main file

--- src/main/java/org/Angular/Main.java
+++ src/main/java/org/Angular/Main.java
@@ -1,23 +1,9 @@
 package org.Angular;
 
-import AST.Array;
-import AST.Value;
-
-import java.util.Arrays;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Array simpleArray = new Array(Arrays.asList(
-                new Value(10),
-                new Value("Hello"),
-                new Value(true)
-        ));
-        System.out.println(simpleArray);
 
-        Array nestedArray = new Array(Arrays.asList(
-                new Value(simpleArray),
-                new Value(42)
-        ));
-        System.out.println(nestedArray);
+    public static void main(String[] args) throws IOException {
     }
 }
